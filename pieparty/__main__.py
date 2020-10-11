e614c015b7704c1dbd82013f7717d9a599799bda
amcruise <53617152+amcruise@users.noreply.github.com>
2020-10-11 17:18:52 -0400

    Add files via upload

---
+++ pieparty/__main__.py
@@ -0,0 +1,12 @@
+import sys
+
+from .pieparty import pieparty
+
+def main(args=None):
+    if args is None:
+        args = sys.argv[1:]
+        pieparty(args)
+
+
+if __name__ == '__main__':
+    sys.exit(main())
